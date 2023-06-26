9927a72f9c03bdf1e9aef0e936ba5e8cfbb74cc5
Vee-nimation <112233334+Vee-nimation@users.noreply.github.com>
2023-06-26 17:35:47 -0500

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
+  const result = [];
   for (const idx of array) {
-    newArray.push(`${appendString}${array[idx]}`);
+    result.push(appendString + idx);
   }
-
-  return newArray;
+  return result;
 }
